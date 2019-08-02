5009e9302bc180c8ad31abc2bc33a52cdcb6cd14
Ademola Hussain <demola.hussainin@gmail.com>
2019-08-02 07:59:16 +0100

    Implement React's useState Hook Display conditional messages to screen viz react's useState hook

--- App.js
+++ App.js
@@ -6,69 +6,42 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
+  Button,
   View,
   Text,
-  StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
+  const [numberOfClicks, handleNumberOfClicks] = useState(0);
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+   <View style={styles.body}>
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>
+        {`You are a ${numberOfClicks > 5 ? 'Superstar' : 'Rockstar'}`}
+      </Text>
+      <Text style={styles.sectionDescription}>
+        {
+          `You have clicked the button ${numberOfClicks} 
+          ${(numberOfClicks > 1) ? 'times' : 'time'}`
+        }
+      </Text>
+    </View>
+    <View style={styles.sectionContainer}>
+      <Button
+        onPress={(arg) => { handleNumberOfClicks(numberOfClicks + 1); }}
+        title='Click Me'
+        color='#841584'
+        accessibilityLabel='Click more than 5 times to change your reputation'
+      />
+    </View>
+   </View>
   );
 };
 
